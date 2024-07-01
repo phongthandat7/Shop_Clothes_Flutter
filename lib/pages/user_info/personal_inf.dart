f484059d76c86b83e26aee55eca012cee151e91e
Tuna0910 <truongiatuan0303@gmail.com>
2024-07-01 12:12:19 +0700

    Create personal_inf.dart

---
+++ lib/pages/user_info/personal_inf.dart
@@ -0,0 +1,167 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: ProfileContainer(),
+        ),
+      ),
+    );
+  }
+}
+
+class ProfileContainer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: const Color(0xFFFFFFFF),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(1, 2, 1, 34),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              margin: const EdgeInsets.only(bottom: 59.5),
+              decoration: const BoxDecoration(
+                color: Color(0xFFFFFFFF),
+                border: Border(
+                  bottom: BorderSide(
+                    color: Color(0xEEEEEEEE),
+                    width: 1,
+                  ),
+                ),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(32.8, 51, 0, 13),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.symmetric(vertical: 9.5),
+                      width: 24,
+                      height: 24,
+                      child: SvgPicture.asset(
+                        'assets/vectors/vector_13_x2.svg',
+                      ),
+                    ),
+                    Text(
+                      'PERSONAL INFORMATION',
+                      style: GoogleFonts.poppins(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 24,
+                        color: const Color(0xFF000000),
+                      ),
+                    ),
+                    const SizedBox(width: 24), // To balance the layout
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(bottom: 15.5),
+              decoration: const BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Color(0x40000000),
+                    offset: Offset(0, 4),
+                    blurRadius: 2,
+                  ),
+                ],
+              ),
+              child: SvgPicture.asset(
+                'assets/vectors/ellipse_101_x2.svg',
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 25, vertical: 104),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  _buildInfoField('NAME', 'Shaun'),
+                  _buildInfoField('BIRTHDAY', '29/04/2003'),
+                  _buildInfoField('EMAIL', 'shaunthesheep@gmail.com'),
+                  _buildInfoField('PASSWORD', '************'),
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 24),
+              decoration: BoxDecoration(
+                color: const Color(0xFF11456E),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                  child: Text(
+                    'SAVE',
+                    style: GoogleFonts.poppins(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 24,
+                      color: const Color(0xFFFFFFFF),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildInfoField(String title, String value) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 5),
+      padding: const EdgeInsets.symmetric(vertical: 14),
+      decoration: const BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            color: Color(0xFF000000),
+            width: 1,
+          ),
+        ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            title,
+            style: GoogleFonts.poppins(
+              fontWeight: FontWeight.w500,
+              fontSize: 16,
+              color: const Color(0xFF000000),
+            ),
+          ),
+          Text(
+            value,
+            style: GoogleFonts.poppins(
+              fontWeight: FontWeight.w300,
+              fontSize: 16,
+              color: const Color(0xFF000000),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
